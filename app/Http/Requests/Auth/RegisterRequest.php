b6dbfd47e1a3a2bc526e8563dd5f93372c38d1e4
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-09 20:50:53 +0300

    Update password confirmation rule and add avatar field to registration request

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -14,8 +14,8 @@ class RegisterRequest extends BaseFormRequest
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
             'phone' => ['required', 'phone:AUTO'],
-            'password' => 'required|string|min:6|max:255',
-            // 'country_code' => 'required|string|max:5',
+            'password' => 'required|string|min:6|max:255|confirmed',
+            'avatar' => 'nullable|string|max:100',
         ];
     }
 }
