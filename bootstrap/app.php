0817e9cd390ebd2e2a3f0e380ff25fac6c54a371
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-09 14:02:18 +0300

    Add handling for NotFoundHttpException in API routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -36,5 +37,16 @@ return Application::configure(basePath: dirname(__DIR__))
                 ], 401);
             }
         });
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Route not found.',
+                    'status' => 404,
+                ], 404);
+            }
+        });
     })
+
     ->create();
