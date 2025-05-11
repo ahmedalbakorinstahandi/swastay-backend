458410c9001e8aca70307382f16e6da8d03bf572
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-11 16:11:52 +0300

    fix: Include api_general.php in routes for improved functionality

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ require_once __DIR__ . '/api_auth.php';
 require_once __DIR__ . '/api_guest.php';
 require_once __DIR__ . '/api_admin.php';
 require_once __DIR__ . '/api_host.php';
+require_once __DIR__ . '/api_general.php';
