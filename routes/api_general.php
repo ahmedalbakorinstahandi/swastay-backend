ae667a785c5f63d2f99b6ae771afb47b570b385b
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-11 17:35:45 +0300

    fix: Correct endpoint path for image upload in API routes

--- routes/api_general.php
+++ routes/api_general.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'general'], function () {
-    Route::post('upload-image', [ImageController::class, 'uploadImage']);
+    Route::post('images/upload', [ImageController::class, 'uploadImage']);
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
 });
