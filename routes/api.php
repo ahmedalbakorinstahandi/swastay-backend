ddadda6831e13a53c21b1d67b45c8ad18ff758ad
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-14 16:04:49 +0300

    feat: add user API route to enhance user management functionality

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ Route::middleware(SetLocaleMiddleware::class)->group(function () {
     require_once __DIR__ . '/api_guest.php';
     require_once __DIR__ . '/api_admin.php';
     require_once __DIR__ . '/api_host.php';
+    require_once __DIR__ . '/api_user.php';
     require_once __DIR__ . '/api_general.php';
 });
