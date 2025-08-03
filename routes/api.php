6af299e4063cb78753d2a29048c239f78898ae1d
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-08-03 23:28:10 +0300

    refactor: update namespace for DigiBankarWebhookController and correct import in api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\DigiBankarWebhookController;
 use App\Http\Middleware\SetLocaleMiddleware;
-use App\Models\DigiBankarWebhookController;
 use Illuminate\Support\Facades\Route;
 
 
